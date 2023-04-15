e49ece9c701d13a76e5ba998030078cd19aca1db
Tomás Hernández <hernandeztomas584@gmail.com>
2023-04-15 16:59:36 -0300

    feat: clicking each category item now its putting in the URL to use filters

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -1,10 +1,14 @@
 'use client';
-import { TbBeach } from 'react-icons/tb';
-import { GiWindmill } from 'react-icons/gi';
+import { TbBeach, TbMountain, TbPool } from 'react-icons/tb';
+import { GiBarn, GiBoatFishing, GiCactus, GiCastle, GiCaveEntrance, GiForestCamp, GiIsland, GiWindmill } from 'react-icons/gi';
 import { MdOutlineVilla } from 'react-icons/md';
+import { FaSkiing } from 'react-icons/fa';
+import { BsSnow } from 'react-icons/bs';
+import { IoDiamond } from 'react-icons/io5';
 
 import Container from "../Container";
 import CategoryBox from './CategoryBox';
+import { usePathname, useSearchParams } from 'next/navigation';
 
 export const categories = [
     {
@@ -22,10 +26,81 @@ export const categories = [
         icon: MdOutlineVilla,
         description: 'This property is modern!'
     },
+    {
+        label: 'Countryside',
+        icon: TbMountain,
+        description: 'This property is in the countryside!'
+    },
+    {
+        label: 'Pools',
+        icon: TbPool,
+        description: 'This property has a pool!'
+    },
+    {
+        label: 'Islands',
+        icon: GiIsland,
+        description: 'This property is on an island!'
+    },
+    {
+        label: 'Lake',
+        icon: GiBoatFishing,
+        description: 'This property is close to a lake!'
+    },
+    {
+        label: 'Skiing',
+        icon: FaSkiing,
+        description: 'This property has skiing activies!'
+    },
+    {
+        label: 'Castles',
+        icon: GiCastle,
+        description: 'This property is in a castle!'
+    },
+    {
+        label: 'Camping',
+        icon: GiForestCamp,
+        description: 'This property has camping activities!'
+    },
+    {
+        label: 'Arctic',
+        icon: BsSnow,
+        description: 'This property has camping activities!'
+    },
+    {
+        label: 'Cave',
+        icon: GiCaveEntrance,
+        description: 'This property is in a cave!'
+    },
+    {
+        label: 'Desert',
+        icon: GiCactus,
+        description: 'This property is in the desert!'
+    },
+    {
+        label: 'Barns',
+        icon: GiBarn,
+        description: 'This property is in the barn!'
+    },
+    {
+        label: 'Lux',
+        icon: IoDiamond,
+        description: 'This property is luxurious!'
+    }
 
 ]
 
 export const Categories = () => {
+
+  const params = useSearchParams();
+  const category = params?.get('category');
+  const pathname = usePathname();
+
+  const isMainPage = pathname === '/';
+
+  if(!isMainPage){
+    return null;
+  }
+
   return (
     <Container>
         <div 
@@ -43,7 +118,7 @@ export const Categories = () => {
                     <CategoryBox 
                         key={item.label} 
                         label={item.label}
-                        description={item.description} 
+                        selected={category === item.label} 
                         icon={item.icon} 
                     />
                 ))
--- app/components/navbar/CategoryBox.tsx
+++ app/components/navbar/CategoryBox.tsx
@@ -2,9 +2,10 @@
 import { useRouter, useSearchParams } from "next/navigation";
 import { useCallback } from "react";
 import { IconType } from "react-icons";
+import qs from 'query-string';
 
 interface CategoryBoxProps {
-    description: string;
+    description?: string;
     icon: IconType;
     label: string;
     selected?: boolean;
@@ -20,8 +21,28 @@ const CategoryBox:React.FC<CategoryBoxProps> = ({
 
   const handleClick = useCallback(() =>{
     let currentQuery = {};
+    if(params){
+        currentQuery = qs.parse(params.toString());
+    }
     
-  }, [])
+    const updatedQuery: any = {
+        ...currentQuery,
+        category: label
+    }
+
+    if(params?.get('category') === label){
+        delete updatedQuery.category;
+    }
+
+    const url = qs.stringifyUrl({
+        url: '/',
+        query: updatedQuery
+    }, {skipNull: true});
+    
+
+    router.push(url);
+
+  }, [label, params, router])
 
   return (
     <div 
@@ -39,6 +60,7 @@ const CategoryBox:React.FC<CategoryBoxProps> = ({
         ${selected ? 'border-b-neutral-800': 'border-transparent'}
         ${selected ? 'text-neutral-800' : 'text-neutral-500'}
         `}
+        onClick={handleClick}
     >
         <Icon size={26} />
         <div className="font-medium text-sm">
