5f20cc0aa657c497e1d4095c597d4a9bc3bd8dc4
Tomás Hernández <hernandeztomas584@gmail.com>
2023-04-16 20:33:01 -0300

    add: Cloudinary Image uploader read

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -9,6 +9,7 @@ import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
 import dynamic from "next/dynamic";
 import Counter from "../inputs/Counter";
+import ImageUpload from "../inputs/ImageUpload";
 
 enum STEPS {
   CATEGORY = 0,
@@ -52,6 +53,7 @@ const RentModal = () =>{
   const guestCount = watch('guestCount');
   const roomCount = watch('roomCount'); 
   const bathroomCount = watch('bathroomCount'); 
+  const imageSrc = watch('imageSrc'); 
 
   //Re-load map dynamically each time we change location
   const Map = useMemo(() => dynamic(() => import('../Map'), {
@@ -168,6 +170,21 @@ const RentModal = () =>{
             value={bathroomCount}
             onChange={(value) => setCustomValue('bathroomCount', value)}
          />
+      </div>
+    )
+  }
+
+  if(step === STEPS.IMAGES){
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Añade una foto de tu propiedad"
+          subtitle="¡Muéstrale a los huéspedes como luce tu propiedad!"
+        />
+        <ImageUpload
+          value={imageSrc}
+          onChange={(value) => setCustomValue('imageSrc', value)}
+        />
       </div>
     )
   }
